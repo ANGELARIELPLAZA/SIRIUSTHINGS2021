958235c6cb0bc88c7bee04f385ba19ed852e42bd
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-25 11:50:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-#mqtt = mqtt.Client()
-#mqtt.connect("localhost", 1883, 60)
-#mqtt.loop_start()
-# Mesa 1                 
-topic1 = "mesa1"   
+# mqtt = mqtt.Client()
+# mqtt.connect("localhost", 1883, 60)
+# mqtt.loop_start()
+# Mesa 1
+topic1 = "mesa1"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -17,32 +17,31 @@ def home():
 
 @app.route("/jetson", methods=["GET", "POST"])
 def jetson():
-	return render_template('jetson.html')
+      return render_template('jetson.html')
 
 
 @app.route("/arduino", methods=["GET", "POST"])
 def jetson():
-	datos = request.form.to_dict(flat=True)
-  print(datos)
+	    datos = request.form.to_dict(flat=True)
+      print(datos)
   #########################################
-  valores1 = datos.get("mesa1")
+      valores1 = datos.get("mesa1")
   #########################################
-  print(valores1)
-  return render_template('arduino.html')
-
+      print(valores1)
+      return render_template('arduino.html')
 
 @app.route("/esp8266", methods=["GET", "POST"])
 def jetson():
-	datos = request.form.to_dict(flat=True)
-  print(datos)
-  #########################################
-  valores1 = datos.get("mesa1")
-  #########################################
-  print(valores1)
+	    datos = request.form.to_dict(flat=True)
+      print(datos)
+      #########################################
+      valores1 = datos.get("mesa1")
+      #########################################
+      print(valores1)
 
-	return render_template('esp8266.html')
+	    return render_template('esp8266.html')
 
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8181, debug=True)
+      app.run(host='0.0.0.0', port=8181, debug=True)
