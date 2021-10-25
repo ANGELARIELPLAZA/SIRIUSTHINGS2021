bc88c14a9abbda4d2c1eeb981603439b0b1682eb
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-25 11:55:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
-import paho.mqtt.client as mqtt
-from flask import Flask, render_template, request
+from flask import Flask,render_template,url_for,request,redirect, make_response,send_file
 
 app = Flask(__name__)
 
-# mqtt = mqtt.Client()
-# mqtt.connect("localhost", 1883, 60)
-# mqtt.loop_start()
-# Mesa 1
-topic1 = "mesa1"
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+       
+    return render_template('index.html')
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-      return render_template('raspi.html')
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Temperature, Humidity]
+
+    Temperature = random() * 100
+    Humidity = random() * 55
+
+    data = [time() * 1000, Temperature, Humidity]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+    return response
 
 
-@app.route("/jetson", methods=["GET", "POST"])
+@app.route('/jetson')
 def jetson():
-      return render_template('jetson.html')
+    
+    return render_template('jetson.html')
+
+@app.route('/raspi')
+def raspi():
+    
+    return render_template('raspi.html')
+    
+@app.route('/arduino')
+def arduino():
+    
+    return render_template('arduino.html')
+    
 
 
-@app.route("/arduino", methods=["GET", "POST"])
-def jetson():
-	    datos = request.form.to_dict(flat=True)
-      print(datos)
-  #########################################
-      valores1 = datos.get("mesa1")
-  #########################################
-      print(valores1)
-      return render_template('arduino.html')
-
-@app.route("/esp8266", methods=["GET", "POST"])
-def jetson():
-	    datos = request.form.to_dict(flat=True)
-      print(datos)
-      #########################################
-      valores1 = datos.get("mesa1")
-      #########################################
-      print(valores1)
-
-	    return render_template('esp8266.html')
-
-
-
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8181, debug=True)
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
